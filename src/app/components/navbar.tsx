19ac1ef4dbe0edf88f676d1a467642b2865a15fc
gchaconSLIS <gchacon@sliscorp.com>
2024-05-15 17:33:11 +0200

    SearchBar-ClientForm

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -11,88 +11,133 @@ export default function Navbar() {
   return (
     <div className={styles.navContainer}>
         <div>
-             <div className={styles.welcome}>
-                <span>Bienvenido al Banco digital de Caracas</span>
+            <div className={styles.welcome}>
+                <span>Bienvenido al Banco digital de <span className={styles.gradient}>Caracas</span></span>
             </div>
             <List >
-                <span className={styles.navSpans}>General</span>
-                <ListItem
-                //   secondaryAction={
-                //     /* aqui va el elemento que se quiera poner a la derecha */
-                //   }
-                >
-                <ListItemIcon>
-                    <FolderIcon />
-                </ListItemIcon>
-                <ListItemText
-                    primary="Solicitudes"
-                />
-                </ListItem>
-                <ListItem>
+                <div className={styles.blockNav}>
+                    <span className={styles.navSpans}>General</span>
+                    <ListItem
+                    //   secondaryAction={
+                    //     /* aqui va el elemento que se quiera poner a la derecha */
+                    //   }
+                    >
+                    <ListItemIcon>
+                        <FolderIcon />
+                    </ListItemIcon>
                     <ListItemText
-                        primary="Agendar"
-                        />
-                </ListItem>
-                <ListItem>
-                    <ListItemText
-                        primary="Analisis"
+                        primary="Solicitudes"
+                    />
+                    </ListItem>
+                    <ListItem>
+                        <ListItemIcon>
+                            <FolderIcon />
+                        </ListItemIcon>
+                        <ListItemText
+                            primary="Agendar"
                         />
                     </ListItem>
                     <ListItem>
+                        <ListItemIcon>
+                            <FolderIcon />
+                        </ListItemIcon>
+                        <ListItemText
+                            primary="Analisis"
+                        />
+                    </ListItem>
+                    <ListItem>
+                        <ListItemIcon>
+                            <FolderIcon />
+                        </ListItemIcon>
                         <ListItemText
                         primary="Crear productos financieros"
                         />
-                </ListItem >
-                <Divider />
-                <span className={styles.navSpans}>Mi espacio</span>
-                <ListItem
-                //   secondaryAction={
-                //     /* aqui va el elemento que se quiera poner a la derecha */
-                //   }
-                >
-                    <ListItemIcon>
-                        <FolderIcon />
-                    </ListItemIcon>
-                    <ListItemText
-                        primary="Solicitudes"
-                    />
-                </ListItem>
-                <ListItem>
-                    <ListItemText
-                        primary="Agendar"
+                    </ListItem >
+                    <ListItem>
+                        <ListItemIcon>
+                            <FolderIcon />
+                        </ListItemIcon>
+                        <ListItemText
+                        primary="Reclutamiento"
                         />
-                </ListItem>
-                <ListItem>
-                    <ListItemText
-                        primary="Analisis"
+                    </ListItem >
+                    <ListItem>
+                        <ListItemIcon>
+                            <FolderIcon />
+                        </ListItemIcon>
+                        <ListItemText
+                        primary="Projecto"
                         />
-                </ListItem>
-                    
-                <Divider />  
-                <span className={styles.navSpans}>Mi espacio</span>
-                <ListItem
-                //   secondaryAction={
-                //     /* aqui va el elemento que se quiera poner a la derecha */
-                //   }
-                >
-                    <ListItemIcon>
-                        <FolderIcon />
-                    </ListItemIcon>
-                    <ListItemText
-                        primary="Solicitudes"
-                    />
-                </ListItem>
-                <ListItem>
-                    <ListItemText
-                        primary="Agendar"
+                    </ListItem >
+                    <Divider />
+                </div>
+                
+                <div className={styles.blockNav}>
+                    <span className={styles.navSpans}>Mi espacio</span>
+                    <ListItem
+                    //   secondaryAction={
+                    //     /* aqui va el elemento que se quiera poner a la derecha */
+                    //   }
+                    >
+                        <ListItemIcon>
+                            <FolderIcon />
+                        </ListItemIcon>
+                        <ListItemText
+                            primary="Actividad"
                         />
-                </ListItem>
-                <ListItem>
-                    <ListItemText
-                        primary="Analisis"
+                    </ListItem>
+                    <ListItem>
+                        <ListItemIcon>
+                            <FolderIcon />
+                        </ListItemIcon>
+                        <ListItemText
+                            primary="Compartido"
+                            />
+                    </ListItem>
+                    <ListItem>
+                        <ListItemIcon>
+                            <FolderIcon />
+                        </ListItemIcon>
+                        <ListItemText
+                            primary="Privacidad"
+                            />
+                    </ListItem>
+                    <Divider />
+                </div>
+
+                <div className={styles.blockNav}>
+                    <span className={styles.navSpans}>Soporte</span>
+                    <ListItem
+                    //   secondaryAction={
+                    //     /* aqui va el elemento que se quiera poner a la derecha */
+                    //   }
+                    >
+                        <ListItemIcon>
+                            <FolderIcon />
+                        </ListItemIcon>
+                        <ListItemText
+                            primary="Ajustes"
                         />
-                </ListItem>
-                <Divider />      
+                    </ListItem>
+                    <ListItem>
+                        <ListItemIcon>
+                            <FolderIcon />
+                        </ListItemIcon>
+                        <ListItemText
+                            primary="Ayuda"
+                            />
+                    </ListItem>
+                    <ListItem>
+                        <ListItemIcon>
+                            <FolderIcon />
+                        </ListItemIcon>
+                        <ListItemText
+                            primary="Chat"
+                            />
+                    </ListItem>
+                    <Divider /> 
+
+                </div>    
             </List>
         </div>
     </div>
--- src/app/mainLayout/page.tsx
+++ src/app/mainLayout/page.tsx
@@ -1,12 +1,20 @@
 import Auth from "../auth/page";
+import Formulario from "../clients/page";
 import Navbar from "../components/navbar";
+import Searchbar from "../components/searchbar";
 import styles from "../page.module.css";
 
 export default function MainLayout() {
   return (
     <div className={styles.mainLayout}>
-        {/* <Navbar /> */}
-        <Auth />
+        <Navbar />
+        <Searchbar />
+
+        
+
+        {/* <Formulario /> */}
+        {/* <Auth /> */}
+
     </div>
   );
 }
