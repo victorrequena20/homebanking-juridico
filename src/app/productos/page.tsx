97240a7dd7a6496f2e2b5355f60cfa4fe6e66ab0
gchaconSLIS <gchacon@sliscorp.com>
2024-05-28 22:58:48 +0200

    Act

---
+++ src/app/productos/page.tsx
@@ -0,0 +1,10 @@
+import styles from "../product.module.css";
+import { BinBold } from "../../assets/icons";
+
+export default function CreatedProducts() {
+  return (
+    <div className={styles.productsConteiner}>
+      
+    </div>
+  );
+}
