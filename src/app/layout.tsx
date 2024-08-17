3fac1e646fa5851d172a5ca7154728f7a7b5f1da
victorrequena20 <vrequena@sliscorp.com>
2024-08-17 04:29:55 -0400

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,8 +11,16 @@ export default function RootLayout({ children }: any) {
         <title>Banco digital de caracas</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
+        {/* <link
           href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&display=swap"
+          rel="stylesheet"
+        /> */}
+        {/* <link
+          href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap"
+          rel="stylesheet"
+        /> */}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
           rel="stylesheet"
         />
       </head>
