a465f33e69b1ab272710b8bf280147cbe3ddace5
victorrequena20 <vrequena@sliscorp.com>
2024-08-18 18:04:13 -0400

    update

--- src/app/administracion/organizacion/reasignacion-de-creditos-masiva/page.tsx
+++ src/app/administracion/organizacion/reasignacion-de-creditos-masiva/page.tsx
@@ -4,24 +4,8 @@ import Wrapper from "@/components/Wrapper";
 import Breadcrumbs from "@/components/Breadcrumbs";
 import MassiveCreditReallocationForm from "@/modules/administracion/organizacion/components/MassiveCreditReallocationForm";
 import { Stack } from "@mui/material";
-import { getOffices } from "@/services/Office.service";
 
 export default function ReasignacionDeCreditosMasivaPage() {
-  const [isLoading, setIsLoading] = React.useState<boolean>(false);
-  const [offices, setOffices] = React.useState<any[]>([]);
-
-  React.useEffect(() => {
-    (async () => {
-      setIsLoading(true);
-      const response = await getOffices();
-      console.log("🚀 ~ response:", response);
-      if (response?.status === 200) {
-        setOffices(response?.data);
-      }
-      setIsLoading(false);
-    })();
-  }, []);
-
   return (
     <Wrapper>
       <Breadcrumbs
