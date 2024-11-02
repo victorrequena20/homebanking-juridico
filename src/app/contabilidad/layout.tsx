2b132618daedee3f13893cb3c05578bced070ef2
Ervin Rodríguez <ervinrodriguez140@gmail.com>
2024-11-02 14:50:59 -0600

    add suspense into layout calls

--- src/app/contabilidad/layout.tsx
+++ src/app/contabilidad/layout.tsx
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { Suspense } from "react";
 import AuthenticatedLayout from "@/components/Layouts/Authenticated.layout";
+import Loader from "@/components/Loader";
 
 export default function layout({ children }: any) {
-  return <AuthenticatedLayout>{children}</AuthenticatedLayout>;
+  return (
+    <Suspense fallback={<Loader size="40" color="#484848" />}>
+      <AuthenticatedLayout>{children}</AuthenticatedLayout>
+    </Suspense>
+  );
 }
--- src/app/notificaciones/layout.tsx
+++ src/app/notificaciones/layout.tsx
@@ -3,7 +3,9 @@ import AuthenticatedLayout from "@/components/Layouts/Authenticated.layout";
 import Loader from "@/components/Loader";
 
 export default function layout({ children }: any) {
-  return <Suspense fallback={<Loader size="40" color="#484848" />}>
-    <AuthenticatedLayout>{children}</AuthenticatedLayout>;
-  </Suspense>;
+  return (
+    <Suspense fallback={<Loader size="40" color="#484848" />}>
+      <AuthenticatedLayout>{children}</AuthenticatedLayout>;
+    </Suspense>
+  );
 }
