78b70bd70cbbd5dc6717599cbb0e8dc77882da98
victorrequena20 <vrequena@sliscorp.com>
2024-11-02 12:12:23 -0400

    update

--- src/app/notificaciones/layout.tsx
+++ src/app/notificaciones/layout.tsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { Suspense } from "react";
 import AuthenticatedLayout from "@/components/Layouts/Authenticated.layout";
+import Loader from "@/components/Loader";
 
 export default function layout({ children }: any) {
-  return <AuthenticatedLayout>{children}</AuthenticatedLayout>;
+  return <Suspense fallback={<Loader size="40" color="#484848" />}>
+    <AuthenticatedLayout>{children}</AuthenticatedLayout>;
+  </Suspense>;
 }
